cbb14402eb9cc1cb7588feae6a914030df1cf034
Stephane_Branly <stephanebranly.pro@gmail.com>
2021-03-21 22:41:43 +0100

    add: doubly linked list

--- linked-lists/ll.hpp
+++ linked-lists/ll.hpp
@@ -2,22 +2,51 @@
 
 // Template class T
 template<class T>
-class Element{
+
+// Simply linked elements
+class SimplyElement{
     private:
-        Element* next;
+        SimplyElement* next;
         T        value; 
     public:
         // Constructor
-        Element(const T& val, Element* nxt=nullptr): value(val), next(nxt)  {}
+        SimplyElement(const T& val, SimplyElement* nxt=nullptr): value(val), next(nxt)  {}
 
         // Getters
-        Element<T>* getNext() const { return next; }
-        const T& getValue() const { return value; }  
+        SimplyElement<T>* next() const { return next; }
+        const T& value() const { return value; }  
 
         // Setters
-        void setNext(Element* nxt) { next = nxt; }
+        void setNext(SimplyElement* nxt) { next = nxt; }
         void setValue(T& val) { value = val; }
 
         // Destructor
-        ~Element() {};
+        ~SimplyElement() {};
 };
+
+// Template class T
+template<class T>
+// Doubly linked elements
+class DoublyElement{
+    private:
+        DoublyElement* next, previous;
+        T              value;
+
+    public:
+        // Constructor
+        DoublyElement(const T& val, DoublyElement* nxt=nullptr, DoublyElement* prv=nullptr): value(val), next(nxt), previous(prv)  {}
+
+        // Getters
+        DoublyElement<T>* next() const { return next; }
+        DoublyElement<T>* previous() const { return previous; }
+        const T& value() const { return value; }  
+
+        // Setters
+        void setNext(DoublyElement* nxt) { next = nxt; }
+        void setPrevious(DoublyElement* prv) { previous = prv; }
+        void setValue(T& val) { value = val; }
+
+        // Destructor
+        ~DoublyElement() {};
+
+}
